6e52906222b00d286541e8acade59026400422af
nader.alfakesh <nader.alfakesh@gmail.com>
2020-05-19 05:26:41 +0300

    Updates

---
+++ precache-manifest.472bfdee2a34cbf1248e751db53fc03a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c373730ace9acb663be5232ecda92f53",
+    "url": "/movie-explorer/index.html"
+  },
+  {
+    "revision": "9ea02730c629ceaa69ca",
+    "url": "/movie-explorer/static/css/main.89a3e2db.chunk.css"
+  },
+  {
+    "revision": "db638f3da1048b7a4075",
+    "url": "/movie-explorer/static/js/2.8fa68947.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/movie-explorer/static/js/2.8fa68947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea02730c629ceaa69ca",
+    "url": "/movie-explorer/static/js/main.18d996c2.chunk.js"
+  },
+  {
+    "revision": "fdfa551c42d3f1abaf0d",
+    "url": "/movie-explorer/static/js/runtime-main.dc9fdfde.js"
+  },
+  {
+    "revision": "ae23a172292167edccf4f5c1acbe60a3",
+    "url": "/movie-explorer/static/media/headerBG.ae23a172.jpg"
+  }
+]);
